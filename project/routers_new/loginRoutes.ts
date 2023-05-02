279daddeaa42b60e6bf3d4d6fc6b690f7f010ade
WilsonLin <wilsonlin@gmail.com>
2023-05-02 17:37:17 +0800

    on9

--- project/routers_new/loginRoutes.ts
+++
@@ -1,8 +0,0 @@
-// import { userLoginController, volLoginController } from "../server";
-// import express from "express";
-
-// export const userLoginRoutes = express.Router();
-// export const volLoginRoutes = express.Router();
-
-// userLoginRoutes.post("/login", userLoginController.userLoginControl);
-// volLoginRoutes.post("/login", volLoginController.volLoginControl);
