d79d688b4a04e021aa4619f4f795a7e36f5231fc
WilsonLin <wilsonlin@gmail.com>
2023-05-05 12:40:11 +0800

    case add event

--- project/routers_new/vol_main_route.ts
+++ project/routers_new/vol_main_route.ts
@@ -20,7 +20,7 @@ vol_main_route.put("/volunteer_post_update/:id", volPostController.vol_post_edit
 // // from  ../controllers/volunteer/vol_case_controller.ts
 vol_main_route.get("/volunteer_case", volCaseController.vol_case);
 vol_main_route.get("/volunteer_case_info/:id", volCaseController.vol_case_info);
-vol_main_route.post("/volunteer_case_accept", volCaseController.vol_case_accept);
-vol_main_route.post("/volunteer_case_reject", volCaseController.vol_case_reject);
+vol_main_route.put("/volunteer_case_accept", volCaseController.vol_case_accept);
+vol_main_route.put("/volunteer_case_reject", volCaseController.vol_case_reject);
 vol_main_route.get("/volunteer_case_event/:id", volCaseController.vol_case_event);
-vol_main_route.put("/volunteer_case_update/:id", volCaseController.vol_case_edit);
+vol_main_route.post("/volunteer_case_update/", volCaseController.vol_case_edit);
