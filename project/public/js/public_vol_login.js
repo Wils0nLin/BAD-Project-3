3a800c62566fa756dfed59d4f47cab850259addc
WilsonLin <wilsonlin@gmail.com>
2023-05-08 12:11:08 +0800

    file rename

---
+++ project/public/js/public_vol_login.js
@@ -0,0 +1,23 @@
+// ---------- Login Part ----------//
+const form = document.querySelector("#login-form");
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const username = form.username.value;
+  const password = form.password.value;
+  console.log(username);
+  console.log(password);
+
+  // ---------- Login Info Submission ----------//
+  const resp = await fetch("/public_volunteer_login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password }),
+  });
+
+  if (resp.status === 200) {
+    window.location = "/volunteer_index.html";
+  } else {
+    const data = await resp.json();
+    alert(data.message);
+  }
+});
