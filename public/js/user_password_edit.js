0c69b763caa789c9930d72513004dc8474232b8d
WilsonLin <wilsonlin@gmail.com>
2023-05-09 11:41:46 +0800

    fk11

--- public/js/user_password_edit.js
+++ public/js/user_password_edit.js
@@ -5,6 +5,7 @@ window.onload = () => {
 async function userProfile() {
     const resp = await fetch("/user_profile");
     const profile = await resp.json();
+    console.log(profile);
 
     let userNameHtml = "";
     document.querySelector("#user-name").innerHTML = "";
@@ -18,3 +19,41 @@ async function userProfile() {
 
     const formData = document.querySelector("#user-info");
 }
+
+function check_password_again() {
+    const passwordInput = document.getElementById("u_password").value;
+    const confirmInput = document.getElementById("u_password_check").value;
+
+    if (passwordInput != confirmInput) {
+        alert("兩次密碼須一致");
+    } else {
+        return passwordInput, confirmInput;
+    }
+}
+
+const formData = document.querySelector("#user-info");
+
+formData.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const formData = event.target;
+    const u_password = form.u_password.value;
+    let formObject = {
+        u_password,
+    };
+
+    // console.log(userProfile.id);
+    const response = await fetch(`/public_user_reset/${profile.id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formObject),
+    });
+
+    if (response.status === 200) {
+        const data = await response.json();
+        window.location = "/public_user_login.html";
+        console.log(data);
+        alert("修改成功");
+    } else {
+        alert("未能修改");
+    }
+});
